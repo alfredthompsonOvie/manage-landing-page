d3544ddcbc3197cd2d686a0bf3593ae80c78a4ba
thompson <alfredthompsonovie@gmail.com>
2022-10-29 13:00:31 +0100

    about section

---
+++ src/components/AppAbout.vue
@@ -0,0 +1,52 @@
+<template>
+	<section class="about">
+		<div class="about__heading">
+			<h2 class="title title--subHeading">What’s different about Manage?</h2>
+			<p class="text text--main">
+				Manage provides all the functionality your team needs, without the
+				complexity. Our software is tailor-made for modern digital product
+				teams.
+			</p>
+		</div>
+		<div class="about__contents">
+			<div class="about__content__card">
+				<div class="about__content__card__titleBg"></div>
+				<p class="about__content__card__number">01</p>
+				<p class="about__content__card__title">Track company-wide progress</p>
+				<p class="text about__content__card__text">
+					See how your day-to-day tasks fit into the wider vision. Go from
+					tracking progress at the milestone level all the way done to the
+					smallest of details. Never lose sight of the bigger picture again.
+				</p>
+			</div>
+			<div class="about__content__card">
+				<div class="about__content__card__titleBg"></div>
+				<p class="about__content__card__number">02</p>
+				<p class="about__content__card__title">Advanced built-in reports</p>
+				<p class="text about__content__card__text">
+					Set internal delivery estimates and track progress toward company
+					goals. Our customisable dashboard helps you build out the reports you
+					need to keep key stakeholders informed.
+				</p>
+			</div>
+			<div class="about__content__card">
+				<div class="about__content__card__titleBg"></div>
+				<p class="about__content__card__number">03</p>
+				<p class="about__content__card__title">
+					Everything you need in one place
+				</p>
+				<p class="text about__content__card__text">
+					Stop jumping from one service to another to communicate, store files,
+					track tasks and share documents. Manage offers an all-in-one team
+					productivity solution.
+				</p>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "AppAbout",
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <template>
   <app-header />
+  <main class="main">
+    <app-about />
+  </main>
 </template>
 
 <script>
 import AppHeader from '../components/AppHeader.vue';
+import AppAbout from '../components/AppAbout.vue';
   export default {
   name: "HomeView",
   components: {
-    AppHeader
+    AppHeader,
+    AppAbout
     }
   }
 </script>
